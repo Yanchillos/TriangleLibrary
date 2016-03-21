0ed41b3a17aeb85c4a4e6a818d60776c51e381c2
Yanchillos <yanchillos@gmail.com>
2016-03-21 22:00:02 +0300

    first commit

---
+++ TriangleLibrary/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleLibrary
+{
+    public class Triangle
+    {
+        public static double GetTriangleArea(double a, double b, double c)
+        {
+            if ((a <= 0) || (b <= 0) || (c <= 0))
+            {
+                Console.WriteLine("Неправильные длины сторон");
+                return -1;
+            }
+
+            var sides = new double[] { a, b, c };
+            Array.Sort(sides);
+
+            if (sides[0] * sides[0] + sides[1] * sides[1] != sides[2] * sides[2])
+            {
+                Console.WriteLine("Стороны с такими длиннами не могут образовывать прямоугольный треугольник");
+                return -1;
+            }
+
+            return sides[0] * sides[1] / 2;
+        }
+    }
+}
